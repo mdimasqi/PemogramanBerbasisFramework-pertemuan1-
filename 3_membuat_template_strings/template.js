771f7aed41b1501fae9fcd7df2ab4f3d4f461716
mdhimasqi <mdhimasqi@gmail.com>
2022-02-16 09:49:06 +0700

    commit pertama

---
+++ 3_membuat_template_strings/template.js
@@ -0,0 +1,6 @@
+let fname = 'Mochammad';
+let lname = 'Dimasqi';
+let age = prompt("Masukkan umur Mochammad Dimasqi");
+// let result = fname + ' ' + lname + 'is' + age + 'years old';
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
